76224680c60205c5334ca409545024a055c6bc47
hozhai <zhaihongmeng@gmail.com>
2026-06-04 21:46:17 -0400

    feat: add hero title

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,64 +1,31 @@
 <script lang="ts">
-
+import { motion } from 'motion-sv';
 </script>
-<svg xmlns="http://www.w3.org/2000/svg" width="440" height="144" class="svgbob"><style>.svgbob line, .svgbob path, .svgbob circle, .svgbob rect, .svgbob polygon {
-    stroke: black;
-    stroke-width: 2;
-    stroke-opacity: 1;
-    fill-opacity: 1;
-    stroke-linecap: round;
-    stroke-linejoin: miter;
-}
-.svgbob text {
-    white-space: pre;
-    fill: black;
-    font-family: Iosevka Fixed, monospace;
-    font-size: 14px;
-}
-.svgbob rect.backdrop {
-    stroke: none;
-    fill: white;
-}
-.svgbob .broken {
-    stroke-dasharray: 8;
-}
-.svgbob .filled {
-    fill: black;
-}
-.svgbob .bg_filled {
-    fill: white;
-    stroke-width: 1;
-}
-.svgbob .nofill {
-    fill: white;
-}
-.svgbob .end_marked_arrow {
-    marker-end: url(#arrow);
-}
-.svgbob .start_marked_arrow {
-    marker-start: url(#arrow);
-}
-.svgbob .end_marked_diamond {
-    marker-end: url(#diamond);
-}
-.svgbob .start_marked_diamond {
-    marker-start: url(#diamond);
-}
-.svgbob .end_marked_circle {
-    marker-end: url(#circle);
-}
-.svgbob .start_marked_circle {
-    marker-start: url(#circle);
-}
-.svgbob .end_marked_open_circle {
-    marker-end: url(#open_circle);
-}
-.svgbob .start_marked_open_circle {
-    marker-start: url(#open_circle);
-}
-.svgbob .end_marked_big_open_circle {
-    marker-end: url(#big_open_circle);
-}
-.svgbob .start_marked_big_open_circle {
-    marker-start: url(#big_open_circle);
-}<!--separator--></style><defs><marker id="arrow" viewBox="-2 -2 8 8" refX="4" refY="2" markerWidth="7" markerHeight="7" orient="auto-start-reverse"><polygon points="0,0 0,4 4,2 0,0"/></marker><marker id="diamond" viewBox="-2 -2 8 8" refX="4" refY="2" markerWidth="7" markerHeight="7" orient="auto-start-reverse"><polygon points="0,2 2,0 4,2 2,4 0,2"/></marker><marker id="circle" viewBox="0 0 8 8" refX="4" refY="4" markerWidth="7" markerHeight="7" orient="auto-start-reverse"><circle cx="4" cy="4" r="2" class="filled"/></marker><marker id="open_circle" viewBox="0 0 8 8" refX="4" refY="4" markerWidth="7" markerHeight="7" orient="auto-start-reverse"><circle cx="4" cy="4" r="2" class="bg_filled"/></marker><marker id="big_open_circle" viewBox="0 0 8 8" refX="4" refY="4" markerWidth="7" markerHeight="7" orient="auto-start-reverse"><circle cx="4" cy="4" r="3" class="bg_filled"/></marker></defs><rect class="backdrop" x="0" y="0" width="440" height="144"/><text x="10" y="12">$$</text><text x="58" y="12">$$</text><text x="90" y="12">$$</text><text x="170" y="12">$$</text><text x="290" y="12">$$$$$$</text><text x="370" y="12">$$$$$$</text><text x="18" y="28">$$$</text><text x="66" y="28">$$</text><text x="178" y="28">$$</text><text x="282" y="28">$$</text><line x1="296" y1="32" x2="312" y2="32" class="solid"/><text x="330" y="28">$$</text><text x="362" y="28">$$</text><line x1="376" y1="32" x2="392" y2="32" class="solid"/><text x="410" y="28">$$</text><text x="18" y="44">$$$$</text><text x="66" y="44">$$</text><text x="98" y="44">$$</text><line x1="136" y1="32" x2="120" y2="64" class="solid"/><text x="130" y="60">$$</text><text x="138" y="44">$$$$$$$</text><line x1="216" y1="32" x2="200" y2="64" class="solid"/><text x="210" y="60">$$</text><text x="218" y="44">$$$$$$</text><text x="290" y="44">$$</text><text x="370" y="44">$$</text><text x="18" y="60">$$</text><text x="42" y="60">$$</text><text x="66" y="60">$$|</text><text x="98" y="60">$$</text><text x="178" y="60">$$</text><text x="258" y="60">$$</text><text x="290" y="60">$$</text><text x="370" y="60">$$</text><text x="18" y="76">$$</text><text x="50" y="76">$$$$</text><text x="98" y="76">$$|</text><text x="130" y="76">$$</text><text x="178" y="76">$$|</text><text x="210" y="76">$$</text><text x="258" y="76">$$</text><text x="290" y="76">$$</text><text x="370" y="76">$$</text><text x="18" y="92">$$</text><text x="58" y="92">$$$</text><text x="98" y="92">$$</text><text x="130" y="92">$$</text><text x="178" y="92">$$</text><text x="210" y="92">$$</text><text x="258" y="92">$$</text><text x="290" y="92">$$</text><text x="338" y="92">$$</text><text x="370" y="92">$$</text><text x="418" y="92">$$</text><text x="18" y="108">$$</text><text x="66" y="108">$$</text><text x="98" y="108">$$</text><text x="138" y="108">$$$$$$$</text><text x="218" y="108">$$$$$$</text><text x="298" y="108">$$$$$$</text><text x="378" y="108">$$$$$$</text><g><line x1="8" y1="0" x2="4" y2="8" class="solid"/><line x1="4" y1="8" x2="4" y2="128" class="solid"/><line x1="4" y1="128" x2="24" y2="128" class="solid"/><line x1="32" y1="112" x2="24" y2="128" class="solid"/></g><g><line x1="56" y1="0" x2="52" y2="8" class="solid"/><line x1="52" y1="8" x2="52" y2="48" class="solid"/></g><g><line x1="88" y1="0" x2="84" y2="8" class="solid"/><line x1="84" y1="8" x2="84" y2="32" class="solid"/><line x1="84" y1="32" x2="104" y2="32" class="solid"/><line x1="112" y1="16" x2="104" y2="32" class="solid"/><line x1="96" y1="32" x2="88" y2="48" class="solid"/></g><g><line x1="168" y1="0" x2="164" y2="8" class="solid"/><line x1="164" y1="8" x2="164" y2="32" class="solid"/></g><g><line x1="288" y1="0" x2="276" y2="24" class="solid"/><line x1="276" y1="24" x2="276" y2="88" class="solid"/><line x1="276" y1="88" x2="256" y2="128" class="solid"/><line x1="116" y1="80" x2="116" y2="104" class="solid"/><line x1="116" y1="104" x2="104" y2="128" class="solid"/><line x1="116" y1="104" x2="128" y2="128" class="solid"/><line x1="196" y1="80" x2="196" y2="104" class="solid"/><line x1="196" y1="104" x2="184" y2="128" class="solid"/><line x1="196" y1="104" x2="208" y2="128" class="solid"/><line x1="128" y1="128" x2="184" y2="128" class="solid"/><line x1="208" y1="128" x2="256" y2="128" class="solid"/><line x1="84" y1="112" x2="84" y2="128" class="solid"/><line x1="84" y1="128" x2="104" y2="128" class="solid"/></g><g><line x1="368" y1="0" x2="356" y2="24" class="solid"/><line x1="356" y1="24" x2="356" y2="88" class="solid"/><line x1="356" y1="88" x2="336" y2="128" class="solid"/><line x1="276" y1="96" x2="276" y2="104" class="solid"/><line x1="276" y1="104" x2="288" y2="128" class="solid"/><line x1="288" y1="128" x2="336" y2="128" class="solid"/></g><g><line x1="320" y1="32" x2="328" y2="48" class="solid"/><line x1="328" y1="48" x2="344" y2="48" class="solid"/><line x1="352" y1="32" x2="344" y2="48" class="solid"/></g><g><line x1="400" y1="32" x2="408" y2="48" class="solid"/><line x1="408" y1="48" x2="424" y2="48" class="solid"/><line x1="432" y1="32" x2="424" y2="48" class="solid"/></g><g><line x1="144" y1="64" x2="164" y2="64" class="solid"/><line x1="164" y1="64" x2="164" y2="96" class="solid"/></g><g><line x1="224" y1="64" x2="240" y2="64" class="solid"/><line x1="240" y1="64" x2="244" y2="72" class="solid"/><line x1="244" y1="72" x2="244" y2="96" class="solid"/></g><g><line x1="84" y1="64" x2="84" y2="104" class="solid"/><line x1="84" y1="104" x2="72" y2="128" class="solid"/><line x1="32" y1="80" x2="56" y2="128" class="solid"/><line x1="56" y1="128" x2="72" y2="128" class="solid"/></g><g><line x1="356" y1="96" x2="356" y2="104" class="solid"/><line x1="356" y1="104" x2="368" y2="128" class="solid"/><line x1="432" y1="96" x2="416" y2="128" class="solid"/><line x1="368" y1="128" x2="416" y2="128" class="solid"/></g></svg>
+
+<motion.div
+  initial={{
+    opacity: 0,
+    translateY: 30,
+  }}
+  animate={{
+    opacity: 1,
+    translateY: 0
+  }}
+  transition={{
+    delay: 0.2,
+    duration: 0.8,
+    ease: (t: number) => Math.floor(t * 5) / 5
+  }}
+  class="text-primary font-bold"
+>
+  <pre>
+        /$$ /$$   /$$ /$$       /$$            /$$$$$$   /$$$$$$       /$$ /$$   
+       /$$/| $$$ | $$|__/      | $$           /$$__  $$ /$$__  $$     /$$/|  $$  
+      /$$/ | $$$$| $$ /$$  /$$$$$$$  /$$$$$$ | $$  \__/| $$  \__/    /$$/  \  $$ 
+     /$$/  | $$ $$ $$| $$ /$$__  $$ /$$__  $$| $$      | $$         /$$/    \  $$
+    |  $$  | $$  $$$$| $$| $$  | $$| $$  \ $$| $$      | $$        /$$/      /$$/
+     \  $$ | $$\  $$$| $$| $$  | $$| $$  | $$| $$    $$| $$    $$ /$$/      /$$/ 
+      \  $$| $$ \  $$| $$|  $$$$$$$|  $$$$$$/|  $$$$$$/|  $$$$$$//$$/      /$$/  
+       \__/|__/  \__/|__/ \_______/ \______/  \______/  \______/|__/      |__/   
+    </pre>
+</motion.div>
