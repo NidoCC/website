b84932f37353076bfd1d7c039defcd8d5bf643d0
hozhai <zhaihongmeng@gmail.com>
2026-06-05 21:08:12 -0400

    chore: add prettier-plugin-svelte to prettierrc

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,16 +1,16 @@
 /** @type {import("prettier").Config} */
 export default {
-  tabWidth: 2,
-  semi: true,
-  useTabs: false,
+    tabWidth: 2,
+    semi: true,
+    useTabs: false,
 
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+    plugins: ["prettier-plugin-astro", "prettier-plugin-svelte"],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
 };
