1a6ea8f34da6c136aa8418d616e55bdc76ccb311
hozhai <zhaihongmeng@gmail.com>
2026-06-08 11:36:46 -0400

    feat: change font to geist and geist mono

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
 // @ts-check
-import mdx from '@astrojs/mdx';
-import svelte from '@astrojs/svelte';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   fonts: [
     {
       provider: fontProviders.fontsource(),
-      name: 'Space Grotesk',
-      cssVariable: '--font-space-grotesk',
+      name: "Geist",
+      cssVariable: "--font-geist",
     },
     {
       provider: fontProviders.fontsource(),
-      name: 'Space Mono',
-      cssVariable: '--font-space-mono',
+      name: "Geist Mono",
+      cssVariable: "--font-geist-mono",
     },
   ],
 
